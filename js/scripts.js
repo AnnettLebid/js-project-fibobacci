fe735076a4f3562c3f87343078fcf7f5afb4e433
Anna <annalez16@gmail.com>
2020-06-11 09:34:45 +0300

    Add function getUserInput

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 const fibonacciNumber = (num) => {
-  let prevNum1 = 0, prevNum2 = 1;
+  let prevNum1 = 0,
+    prevNum2 = 1;
   let result = 1;
-  for (let i = 2; i<= num; i++) {
+  for (let i = 2; i <= num; i++) {
     result = prevNum1 + prevNum2;
     prevNum1 = prevNum2;
     prevNum2 = result;
   }
-  return result;  
+  return result;
 };
 
-console.log(fibonacciNumber(5));
+// console.log(fibonacciNumber(5));
 
-document.querySelector(".number").innerHTML = 5;
-document.querySelector(".result").innerHTML = fibonacciNumber(5);
+let btn = document.getElementById("button");
+
+function getUserInput() {
+  let userInput = document.querySelector(".input").value;  
+  console.log(userInput);
+}
+
+btn.addEventListener("click", getUserInput);
+
+// document.querySelector(".number").innerHTML = 5;
+// document.querySelector(".result").innerHTML = fibonacciNumber(5);
