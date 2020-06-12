32d59c2cafef52d3a270a5fb13bb8b2fd0998cdd
Anna <annalez16@gmail.com>
2020-06-12 08:22:43 +0300

    Add fetch function and gitirnore file

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,42 @@
-const fibonacciNumber = (num) => {
-  let prevNum1 = 0,
-    prevNum2 = 1;
-  let result = 1;
-  for (let i = 2; i <= num; i++) {
-    result = prevNum1 + prevNum2;
-    prevNum1 = prevNum2;
-    prevNum2 = result;
-  }
-  return result;
-};
+// function fibonacciNumber(num) {
+//   let prevNum1 = 0,
+//     prevNum2 = 1;
+//   let result = 1;
+//   for (let i = 2; i <= num; i++) {
+//     result = prevNum1 + prevNum2;
+//     prevNum1 = prevNum2;
+//     prevNum2 = result;
+//   }
+//   console.log(result);
+//   document.getElementById("result").innerText = result;
+// }
 
-// console.log(fibonacciNumber(5));
+// function getUserInput() {
+//   let userInput = document.getElementById("input").value;
+//   console.log(userInput);
+//   return userInput;
+// }
+
+// let btn = document.getElementById("button");
+// btn.addEventListener("click", function () {
+//   fibonacciNumber(getUserInput());
+// });
 
 let btn = document.getElementById("button");
+btn.addEventListener("click", function () {  
+let number = document.getElementById("input").value;
+console.log(number);
+//   // let number = getUserInput();
+   fetch(`http://localhost:5050/fibonacci/${number}`)
+  .then(response => response.json())
+  .then(data => console.log(data.result));
+ 
+//   // fibonacciNumber(getUserInput());
+});
 
-function getUserInput() {
-  let userInput = document.querySelector(".input").value;  
-  console.log(userInput);
-}
+// function works() {
+//   console.log("I am working");
+// }
 
-btn.addEventListener("click", getUserInput);
 
-// document.querySelector(".number").innerHTML = 5;
-// document.querySelector(".result").innerHTML = fibonacciNumber(5);
+// myFetch();
