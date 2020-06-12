7e9fb8d136bf5bf65de43d84de6877169749c83c
Anna <annalez16@gmail.com>
2020-06-12 17:41:49 +0300

    Add loader

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,30 @@
+const loader = document.querySelector(".loader-ring");
+
 function getUserInput() {
   let userInput = document.getElementById("input").value;
   return userInput;
 }
 
+function loaderOn() {
+  loader.classList.add("show");
+}
+
+function loaderOff() {
+  loader.classList.remove("show");
+}
+
 function calcFibNumber() {
   let btn = document.getElementById("button");
   btn.addEventListener("click", function () {
+    loader.classList.add("show");
     let number = getUserInput();
     fetch(`http://localhost:5050/fibonacci/${number}`)
       .then((response) => response.json())
-      .then((data) => (document.getElementById("result").innerText = data.result)
-      );
+      .then(function (data) {
+        (document.getElementById("result").innerText = data.result);
+        loader.classList.remove("show");
+      });    
   });
 }
 
 calcFibNumber();
-
