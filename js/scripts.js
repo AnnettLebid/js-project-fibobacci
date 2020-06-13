3d6dd4f064b2907d69ee71650b959f042d899355
Anna <annalez16@gmail.com>
2020-06-13 11:59:36 +0300

    get server error text

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,6 @@ function loaderOff() {
   loader.classList.remove("show");
 }
 
-
 function calcFibNumber() {
   let btn = document.getElementById("button");
   btn.addEventListener("click", function () {
@@ -25,14 +24,69 @@ function calcFibNumber() {
       loaderOff();
       return false;
     } else {
-      fetch(`http://localhost:5050/fibonacci/${number}`)
-        .then((response) => response.json())
-        .then(function (data) {
-          document.getElementById("result").innerText = data.result;
-          loaderOff();
-        });
+      fetch(`http://localhost:5050/fibonacci/${number}`).then(function (
+        response
+      ) {
+        if (response.ok) {
+          return response.json().then(function (data) {
+            document.getElementById("result").innerText = data.result;
+            loaderOff();
+          });
+        } else {
+          response.text().then((text) => {
+            console.log(text);
+            loaderOff();
+          });
+        }
+      });
     }
   });
 }
 
 calcFibNumber();
+
+
+
+
+
+
+// fetch(`http://localhost:5050/fibonacci/42`)
+//   .then(handleResponse)
+//   .then(data => console.log(data))
+//   .catch(error => console.log(error))
+
+// function handleResponse (response) {
+//   let contentType = response.headers.get('content-type')
+//   if (contentType.includes('application/json')) {
+//     return handleJSONResponse(response)
+//   } else if (contentType.includes('text/html')) {
+//     return handleTextResponse(response)
+//   }
+// }
+
+// function handleJSONResponse (response) {
+//   return response.json()
+//     .then(json => {
+//       if (response.ok) {
+//         return json
+//       } else {
+//         return Promise.reject(Object.assign({}, json, {
+//           status: response.status,
+//           statusText: response.statusText
+//         }))
+//       }
+//     })
+// }
+// function handleTextResponse(response) {
+//   return response.text().then((text) => {
+//     if (response.ok) {
+//       return json;
+//     } else {
+//       return Promise.reject({
+//         status: response.status,
+//         statusText: response.statusText,
+//         err: text,
+//       });
+//     }
+//   });
+// }
